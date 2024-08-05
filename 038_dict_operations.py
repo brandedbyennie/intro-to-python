770a781fb510ad0898c55146af8f42508a3b9153
Ennie <enniegd@gmail.com>
2024-08-05 11:39:22 +0100

    dictionary: keeping count

--- 038_dict_operations.py
+++ 038_dict_operations.py
@@ -46,9 +46,9 @@ print(letter_counts)
 # is in the dictionary, try uncommenting this code and see
 # what happens:
 
-# letter_counts = {}
-# for letter in text:
-#   letter_counts[letter] = letter_counts[letter] + 1
+#letter_counts = {}
+#for letter in text:
+#letter_counts[letter] = letter_counts[letter] + 1
 
 # In the assignment above, our right hand expression tries
 # to access the value for a key that has not been added yet.
@@ -67,7 +67,14 @@ print("Function: count_words_by_length")
 # Since there are two words of length 3, etc.
 
 def count_words_by_length(words):
-  pass
+  words_count = {}
+  for word in words:
+    word_count = len(word)
+    if word_count not in words_count:
+      words_count[word_count] = 1
+    else:
+      words_count[word_count] = words_count[word_count] + 1
+  return words_count
 
 check_that_these_are_equal(
   count_words_by_length(["hat", "cat", "I", "bird"]),
